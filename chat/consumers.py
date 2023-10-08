6291961e4ceb7fada0bfa5e2cc78d21252293e31
belemaaa <graceitamunoala@gmail.com>
2023-10-08 10:16:37 +0000

    group consumers

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -6,6 +7,20 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         self.room_group_name = 'chat%s' % self.room
 
         await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester': 'tester'
+            }
+        )
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
